622936ca146029c4ca044f212d82a92f2787ed4b
Small Tsai <sell0802@gmail.com>
2025-11-30 17:07:47 +0800

    Update photos.js

--- photos.js
+++ photos.js
@@ -1,251 +1,250 @@
-// Photo data - add your photo paths and captions here
 const photos = [
     {
         src: 'images/photo00001.jpg',
-        caption: 'this is my first photo'
+        caption: 'Tinkoepel'
     },
     {
         src: 'images/photo00002.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Tulips'
     },
     {
         src: 'images/photo00003.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Ice cream'
     },
     {
         src: 'images/photo00004.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Marienberg'
     },
     {
         src: 'images/photo00005.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Würzburg♥'
     },
     {
         src: 'images/photo00006.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Käppele'
     },
     {
         src: 'images/photo00007.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'lovely'
     },
     {
         src: 'images/photo00008.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'PVG'
     },
     {
         src: 'images/photo00009.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Eyelashes'
     },
     {
         src: 'images/photo00010.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Chocolholic'
     },
     {
         src: 'images/photo00011.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Reflection'
     },
     {
         src: 'images/photo00012.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Reflection'
     },
     {
         src: 'images/photo00013.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Begijnhof'
     },
     {
         src: 'images/photo00014.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Begijnhof'
     },
     {
         src: 'images/photo00015.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Keramiek'
     },
     {
         src: 'images/photo00016.jpg',
-        caption: 'Enter your photo caption here'
+        caption: '♥'
     },
     {
         src: 'images/photo00017.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Jan van Eyckplein'
     },
     {
         src: 'images/photo00018.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Plateau'
     },
     {
         src: 'images/photo00019.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Sashuis'
     },
     {
         src: 'images/photo00020.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Smile'
     },
     {
         src: 'images/photo00021.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Sunny'
     },
     {
         src: 'images/photo00022.jpg',
-        caption: 'Enter your photo caption here'
+        caption: '♥'
     },
     {
         src: 'images/photo00023.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Why so serious?'
     },
     {
         src: 'images/photo00024.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Buddha'
     },
     {
         src: 'images/photo00025.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Roof'
     },
     {
         src: 'images/photo00026.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'DT668'
     },
     {
         src: 'images/photo00027.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Fujian Lu'
     },
     {
         src: 'images/photo00028.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Feedback box'
     },
     {
         src: 'images/photo00029.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Alone'
     },
     {
         src: 'images/photo00030.jpg',
-        caption: 'Enter your photo caption here'
+        caption: '€'
     },
     {
         src: 'images/photo00031.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Red'
     },
     {
         src: 'images/photo00032.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Aandacht'
     },
     {
         src: 'images/photo00033.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'NL'
     },
     {
         src: 'images/photo00034.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Owl'
     },
     {
         src: 'images/photo00035.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Peaceful'
     },
     {
         src: 'images/photo00036.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Light'
     },
     {
         src: 'images/photo00037.jpg',
-        caption: 'Enter your photo caption here'
+        caption: ''
     },
     {
         src: 'images/photo00038.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Tulips'
     },
     {
         src: 'images/photo00039.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Cat'
     },
     {
         src: 'images/photo00040.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Würzburg♥'
     },
     {
         src: 'images/photo00041.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Sleeping'
     },
     {
         src: 'images/photo00042.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'This way'
     },
     {
         src: 'images/photo00043.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Do you want this turtle?'
     },
     {
         src: 'images/photo00044.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'River house'
     },
     {
         src: 'images/photo00045.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Stone'
     },
     {
         src: 'images/photo00046.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Be careful'
     },
     {
         src: 'images/photo00047.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Yellow'
     },
     {
         src: 'images/photo00048.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Reality'
     },
     {
         src: 'images/photo00049.jpg',
-        caption: 'Enter your photo caption here'
+        caption: ''
     },
     {
         src: 'images/photo00050.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Cat'
     },
     {
         src: 'images/photo00051.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Hallstatt'
     },
     {
         src: 'images/photo00052.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Okey'
     },
     {
         src: 'images/photo00053.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Am I broken?'
     },
     {
         src: 'images/photo00054.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Donut'
     },
     {
         src: 'images/photo00055.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Kubuswoningen'
     },
     {
         src: 'images/photo00056.jpg',
-        caption: 'Enter your photo caption here'
+        caption: ''
     },
     {
         src: 'images/photo00057.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Dießen'
     },
     {
         src: 'images/photo00058.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Carnival of Venice'
     },
     {
         src: 'images/photo00059.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Carnival of Venice'
     },
     {
         src: 'images/photo00060.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Carnival of Venice'
     },
     {
         src: 'images/photo00061.jpg',
-        caption: 'Enter your photo caption here'
+        caption: 'Peaceful'
     },
     {
         src: 'images/photo00062.jpg',
-        caption: 'Enter your photo caption here'
+        caption: ''
     }
 ];
